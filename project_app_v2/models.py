fa36da4fa82b86e1f823e4bdaabaf88759f5c334
vitya_selivanow <rabbit@mail.ru>
2024-04-08 17:09:43 +0300

    add some panels

--- project_app_v2/models.py
+++ project_app_v2/models.py
@@ -18,10 +18,12 @@ from wagtail.api.v2.serializers import StreamField as StreamFieldSerializer, Bas
 # Create your models here.
 
 class TagSerializer(BaseSerializer):
-    meta_fields=[]
+    meta_fields = []
+
     class Meta:
         fields = '__all__'
         model = Tag
+
 
 class ProjectPage(Page):
     header_html = models.TextField(blank=True, null=True, verbose_name=_('Название компонента'))
@@ -29,14 +31,13 @@ class ProjectPage(Page):
     header_file = models.FileField(upload_to='projects/headers/', blank=True, null=True,
                                    verbose_name=_('Файл превью'))
 
-    description = models.TextField(verbose_name=_('Описание'), null=True,blank=True)
+    description = models.TextField(verbose_name=_('Описание'), null=True, blank=True)
 
     is_light_background_header = models.BooleanField(default=False, verbose_name=_('Светлый фон'))
     column = models.PositiveIntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(3)],
                                          verbose_name=_("Ширина в колонках"))
     tags = models.ManyToManyField(Tag, blank=True, related_name='projects_v2', verbose_name=_('Метки'))
     order = models.PositiveIntegerField(default=0, blank=False, null=False)
-
 
     content = StreamField(
         block_types=(
@@ -54,15 +55,18 @@ class ProjectPage(Page):
     content_panels = Page.content_panels + [
         MultiFieldPanel([
             FieldPanel("header_html"), FieldPanel('is_light_background_header'),
-            FieldPanel('column'), FieldPanel('tags'), FieldPanel('order')
+            FieldPanel('column'), FieldPanel('tags'), FieldPanel('order'), FieldPanel('description'),
+            FieldPanel('header_file')
         ], heading="Базовые настройки страницы"),
         FieldPanel('content')
     ]
 
     api_fields = [
-        APIField('slug'),
-        APIField('header_html'), APIField('is_light_background_header'),
-        APIField('column'), APIField('tags', serializer=TagSerializer(many=True)), APIField('order'), APIField('content', serializer=StreamFieldSerializer()),
+        APIField('title'), APIField('slug'),
+        APIField('header_html'), APIField('is_light_background_header'), APIField('description'),
+        APIField('header_file'),
+        APIField('column'), APIField('tags', serializer=TagSerializer(many=True)), APIField('order'),
+        APIField('content', serializer=StreamFieldSerializer()),
 
     ]
 
